e5a609f547eaedfcbca94f90970540205b121461
choihub <chgpower@gmail.com>
2020-01-13 13:28:44 +0900

    Updates

---
+++ precache-manifest.dc66396a1bec918d86cd0f9d6e4db584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8ad732748113056ada4a207fbddb04",
+    "url": "/test_code/index.html"
+  },
+  {
+    "revision": "b501546cb693c0d92e44",
+    "url": "/test_code/static/css/2.6a917c01.chunk.css"
+  },
+  {
+    "revision": "b501546cb693c0d92e44",
+    "url": "/test_code/static/js/2.77d9bccc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test_code/static/js/2.77d9bccc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82336727aa94117ea3ec",
+    "url": "/test_code/static/js/main.b63d07ef.chunk.js"
+  },
+  {
+    "revision": "d3007514c6b7dbd9c063",
+    "url": "/test_code/static/js/runtime-main.589985bd.js"
+  }
+]);
